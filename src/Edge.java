1a6b740f8cf4de0dec050d1af256b5633a86fe06
larakollokian <lara.kollokian@gmail.com>
2020-04-23 17:46:15 -0400

    added classes for edge and vertex

---
+++ src/Edge.java
@@ -0,0 +1,27 @@
+
+public class Edge {
+	
+	Vertex v1;
+	
+	Vertex v2;
+	
+	double cost;
+	
+	double reliability;
+
+	public Edge(Vertex v1, Vertex v2, double cost, double reliability) {
+		this.v1 = v1;
+		this.v2 = v2;
+		this.cost = cost;
+		this.reliability = reliability;
+	}
+
+	public double getCost() {
+		return cost;
+	}
+
+	public double getReliability() {
+		return reliability;
+	}
+	
+}
---
+++ src/Vertex.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+
+public class Vertex {
+	
+	private int label;
+	
+	ArrayList<Edge> edges;
+	
+	public Vertex(int label) {
+		this.label = label;
+		this.edges = new ArrayList<Edge>();
+	}
+
+	public int getLabel() {
+		return label;
+	}
+
+	public void addEdge(Edge e) {
+		edges.add(e);
+	}
+	
+	public void removeEdge(Edge e) {
+		edges.remove(e);
+	}
+	
+	public Edge getEdgeBetween(Vertex v) {
+		
+		for(Edge e: edges) {
+			if(e.v1 == v || e.v2 == v) {
+				return e;
+			}
+		}
+		return null;
+	}
+
+}
